b57fb9da05507209518ae75c88c836a259974dbd
Soli <solee_auditore@outlook.com>
2026-05-17 20:11:39 +0200

    add codepipeline

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "Soli"
 
     workspaces {
-      name = "website-iac-dev"
+      name = "website-iac"
     }
   }
 }
@@ -11,5 +11,13 @@ terraform {
 module "application" {
   source = "../../application"
 
-  environment = "dev"
+  aws_region               = var.aws_region
+  project_name             = var.project_name
+  environment              = var.environment
+  tags                     = var.tags
+  github_repository_id     = var.github_repository_id
+  github_branch            = var.github_branch
+  website_source_directory = var.website_source_directory
+  website_bucket_name      = var.website_bucket_name
+  github_connection_name   = var.github_connection_name
 }
---
+++ environments/dev/terraform.tfvars
@@ -0,0 +1,12 @@
+aws_region   = "us-east-1"
+project_name = "website"
+environment  = "dev"
+
+tags = {
+  Owner = "soli"
+}
+
+github_repository_id     = "solimovlanova/draft-project1"
+github_branch            = "main"
+website_source_directory = "s3"
+website_bucket_name      = "movlanova.com"
